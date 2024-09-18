6d29ed2ad4f989ce55dd73a8b2f3bbb82cf917e8
David Malášek <davidmalasek@post.cz>
2024-09-18 22:05:37 +0200

    Work on strnstr.c

--- Libft/ft_strnstr.c
+++ Libft/ft_strnstr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dmalasek <dmalasek@student.42.fr>          +#+  +:+       +#+        */
+/*   By: davidmalasek <davidmalasek@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 16:15:29 by dmalasek          #+#    #+#             */
-/*   Updated: 2024/09/16 16:16:25 by dmalasek         ###   ########.fr       */
+/*   Updated: 2024/09/18 22:05:16 by davidmalase      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,31 @@
 
 char	*strnstr(const char *big, const char *little, size_t len)
 {
+	size_t	i;
+	size_t	e;
+	char	first_char;
+	char	first_occ;
+
+	i = 0;
+	e = 0;
+	if (*little == '\0')
+		return (big);
+	first_char = *little;
+	while (i != len && big[i] != '\0')
+	{
+		little = first_char;
+		while (little[e] != '\0')
+		{
+			if (big[i] == little[0])
+			{
+				while (big[i] == little[e])
+				{
+				}
+			}
+		}
+		big++;
+		i++;
+	}
 }
 
 int	main(void)
