d7d75e83ae1429c563af5f0740e4d43880061b42
David Malasek <dmalasek@c1r3s3.42prague.com>
2024-11-13 19:32:26 +0100

    Finished get_next_line

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: davidmalasek <davidmalasek@student.42.f    +#+  +:+       +#+        */
+/*   By: dmalasek <dmalasek@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 12:47:11 by davidmalase       #+#    #+#             */
-/*   Updated: 2024/10/19 15:37:35 by davidmalase      ###   ########.fr       */
+/*   Updated: 2024/11/13 17:16:11 by dmalasek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,55 +31,61 @@ void	append_to_stash(char **stash, char *buffer)
 	char	*new_stash;
 	size_t	total_len;
 
-	if (*stash == NULL)
-	{
-		total_len = ft_strlen(buffer);
-		new_stash = (char *)malloc(total_len + 1);
-		if (!new_stash)
-			return ;
-		new_stash[0] = '\0';
-	}
-	else
-	{
-		total_len = ft_strlen(*stash) + ft_strlen(buffer);
-		new_stash = (char *)malloc(total_len + 1);
-		if (!new_stash)
-			return ;
+	total_len = ft_strlen(buffer);
+	if (*stash)
+		total_len += ft_strlen(*stash);
+	new_stash = (char *)malloc(total_len + 1);
+	if (!new_stash)
+		return ;
+	new_stash[0] = '\0';
+	if (*stash)
 		ft_strlcpy(new_stash, *stash, ft_strlen(*stash) + 1);
-	}
 	ft_strlcat(new_stash, buffer, total_len + 1);
 	free(*stash);
 	*stash = new_stash;
 }
 
-void	trim_stash(char **stash, int newline_index)
+int	read_and_append(int fd, char **stash)
 {
-	char	*new_stash;
-	int		i;
-	int		e;
+	char	*buffer;
+	int		bytes_read;
 
-	i = 0;
-	e = newline_index + 1;
-	new_stash = (char *)malloc(ft_strlen(*stash) - newline_index);
-	if (!new_stash)
-		return ;
-	while ((*stash)[e])
-		new_stash[i++] = (*stash)[e++];
-	new_stash[i] = '\0';
-	free(*stash);
-	*stash = new_stash;
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (-1);
+	while (1)
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read <= 0)
+			break ;
+		buffer[bytes_read] = '\0';
+		append_to_stash(stash, buffer);
+		if (get_newline(*stash) != -1)
+			break ;
+	}
+	free(buffer);
+	return (bytes_read);
 }
 
 char	*extract_line(char **stash)
 {
 	char	*line;
+	char	*new_stash;
 	int		newline_index;
 
 	newline_index = get_newline(*stash);
 	if (newline_index != -1)
 	{
 		line = ft_substr(*stash, 0, newline_index + 1);
-		trim_stash(stash, newline_index);
+		new_stash = ft_substr(*stash, newline_index + 1, ft_strlen(*stash)
+				- newline_index - 1);
+		if (!new_stash)
+		{
+			free(line);
+			return (NULL);
+		}
+		free(*stash);
+		*stash = new_stash;
 	}
 	else
 	{
@@ -92,24 +98,23 @@ char	*extract_line(char **stash)
 
 char	*get_next_line(int fd)
 {
-	char		buffer[BUFFER_SIZE + 1];
 	static char	*stash;
 	int			bytes_read;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	bytes_read = read(fd, buffer, BUFFER_SIZE);
-	while (bytes_read > 0)
+	bytes_read = read_and_append(fd, &stash);
+	if (bytes_read < 0 || !stash)
 	{
-		buffer[bytes_read] = '\0';
-		append_to_stash(&stash, buffer);
-		if (get_newline(stash) != -1)
-			return (extract_line(&stash));
-		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		free(stash);
+		stash = NULL;
+		return (NULL);
 	}
-	if (bytes_read < 0 || stash == NULL)
-		return (free(stash), stash = NULL, NULL);
-	if (ft_strlen(stash) > 0)
-		return (extract_line(&stash));
-	return (free(stash), stash = NULL, NULL);
+	if (ft_strlen(stash) == 0)
+	{
+		free(stash);
+		stash = NULL;
+		return (NULL);
+	}
+	return (extract_line(&stash));
 }
